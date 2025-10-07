b3020873bc5a1aba8ddbd6842fa5e10f64bad6e4
Juhana Kuparinen <juhku@elisanet.fi>
2025-10-07 22:21:41 +0300

    feat: add timer, skip, switcher, dockerfile, compose file

--- README.md
+++ README.md
@@ -2,6 +2,28 @@
 
 `npm run dev -- --open`
 
+or
+
+`docker compose -f "docker-compose.yml" up -d`
+
+or
+
+`docker compose -f "docker-compose.yml" up -d --build`
+
+For docker debug:
+
+```bash
+docker ps # List container ids
+docker exec -it <server_container_id> sh # Exec with a shell into the wad server (id found with previous command)
+
+./app addsuper <email> <password> # Add superuser to the app
+exit # exit container
+```
+
+If you want to sync images easily to server:
+
+`rsync -avzh --delete --progress --stats . user@address:/path/to/static/images/references`
+
 ## TODO:
 
 1. Images
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,60 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let { file } = $props();
-	let current = $state(file);
+	let { data } = $props();
+	let current = $state(data.file);
+	let time = $state(60);
+	let timeOnReset = $state(60);
+	let loading = $state(false);
+	let hours = $state(0);
+	let minutes = $state(1);
+	let seconds = $state(0);
 
 	const getNewImage = async () => {
+		loading = true;
 		const res = await fetch('/api/images');
 		const data = await res.json();
 		current = data.file;
+		time = timeOnReset;
+		loading = false;
+	};
+
+	const setTime = () => {
+		time = seconds + 60 * minutes + 60 * 60 * hours;
+		timeOnReset = time;
 	};
 
 	onMount(() => {
-		const interval = setInterval(() => getNewImage(), 10000);
+		const interval = setInterval(() => time--, 1000);
 
 		return () => clearInterval(interval);
+	});
+
+	$effect(() => {
+		if (time == 0) {
+			getNewImage();
+		}
 	});
 </script>
 
 <div class="flex h-screen items-center justify-center">
-	<div class="w-[40%]">
-		<img src={current} alt="croqui ref" />
+	<div class="flex h-[90%] gap-15">
+		<section class="gap 15 flex w-[25vw] flex-col gap-5 p-2 text-center">
+			<p class="text-6xl">{new Date(time * 1000).toISOString().slice(11, 19)}</p>
+			<button onclick={getNewImage} class="btn btn-primary">SKIP</button>
+			<div class="flex">
+				<input id="h" type="number" class="input" bind:value={hours} min="0" /><span
+					class="text-2xl">:</span
+				>
+				<input id="m" type="number" class="input" bind:value={minutes} min="0" max="59" /><span
+					class="text-2xl">:</span
+				>
+				<input id="s" type="number" class="input" bind:value={seconds} min="0" max="59" />
+			</div>
+			<button onclick={setTime} class="secondary btn">Set time</button>
+		</section>
+		<section class="w-[70vw]">
+			<img class="mx-auto h-[100%]" src={current} alt="croqui ref" />
+		</section>
 	</div>
 </div>
